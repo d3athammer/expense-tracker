8261182efa8fb28452015002b22610c402fb0a7c
ting wei yap <yap.tingwei@gmail.com>
2023-04-03 21:51:33 +0800

    crud using axios

--- src/Component/TestLink.tsx
+++ src/Component/TestLink.tsx
@@ -42,7 +42,8 @@ const TestLink = () => {
       //
     return () => controller.abort();
   },[])
-  // User interface was defined above
+
+  //Delete user
   const deleteUser = (user: User) => {
     const oriUsers = [...users]
     setUsers(users.filter(u =>  u.id !== user.id))
@@ -53,9 +54,39 @@ const TestLink = () => {
         setUsers(oriUsers)
       })
   }
+  //Create user
+  const addUser = () => {
+    const originalUsers = [...users]
+    // to make sure id is more than 0
+    const newUserId = users.length > 0 ? users[users.length - 1].id + 1 : 1;
+    const newUser = {id: newUserId, name: 'Tendermeat'}
+    setUsers([newUser, ...users])
+    // create a POST request
+    axios.post('https://jsonplaceholder.typicode.com/usersx/', newUser)
+      // .then(res => setUsers([res.data, ...users]))
+      // or destructure res.data
+      .then(({ data: savedUser }) => setUsers([savedUser, ...users]) )
+      .catch(err => {
+        setError(err.message)
+        setUsers(originalUsers)
+      })
+  }
+  //Updating user
+  const updateUser = (user: User) => {
+    const originalUsers = [...users]
+    const updatedUser = {...user, name: user.name + '!'}
+    setUsers(users.map( u => u.id === user.id ? updatedUser : u))
+
+    axios.patch(`https://jsonplaceholder.typicode.com/users/${user.id}`)
+      .catch(err => {
+        setError(err.message)
+        setUsers(originalUsers)
+      })
+  }
 
   return (
     <>
+    <button className="btn btn-primary" onClick={addUser}>Add</button>
      {error && <p className="text-danger">{error}</p>}
      {/* display loading spinner while loading */}
      {isLoading && <div className="spinner-border"></div>}
@@ -63,7 +94,10 @@ const TestLink = () => {
       <ul className="list-group">
         {users.map((user) =>
         <li className="list-group-item d-flex justify-content-between" key={user.id}>{user.name}
+        <div className="">
+        <button className="btn btn-secondary mx-1" onClick={() => updateUser(user)}>Update</button>
         <button className="btn btn-outline-danger" key={user.id} onClick={() => deleteUser(user)}>Delete</button>
+        </div>
         </li>
         )}
       </ul>
